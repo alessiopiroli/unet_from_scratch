60bdad78a68dcfc3baa93f3cf032ecc43f5f4a29
Alessio <alessiopiroli1@gmail.com>
2025-11-15 23:14:50 +0100

    initial training logic

--- train.py
+++ train.py
@@ -4,14 +4,58 @@ from unet.utils.misc import load_config
 import torch
 from unet.dataset.voc_dataset import VOCSegmentationDataset
 from torch.utils.data import DataLoader
+from unet.utils.misc import plot_images_to_file
+import torch.nn as nn
+
+###########################################
+import debugpy
+debugpy.listen(('localhost', 6001))
+print('Waiting for debugger attach...')
+debugpy.wait_for_client()
+###########################################
+
+def post_processor(output):
+    output_sm = torch.nn.functional.softmax(output, 1)
+    output_idxs = torch.argmax(output_sm, 1)
+    return output_idxs
+
+def compute_loss(output, labels):
+    loss_fn = nn.CrossEntropyLoss(reduction='none')
+    loss = loss_fn(output, labels)
+    mask_bg = labels == 0
+    mask_fg = labels != 0
+    loss_bg = loss[mask_bg]*0.1
+    loss_fg = loss[mask_fg]*10
+    loss_bg = torch.mean(loss_bg)
+    loss_fg = torch.mean(loss_bg)
+    total_loss = loss_bg + loss_fg
+    return total_loss
+
+def train(model, train_dataloader):
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+    model = model.cuda()
+    model.train()
+    n_epochs = 10
+    for epoch in range(n_epochs):
+        for img, labels in train_dataloader:
+            img, labels = img.cuda(), labels.cuda()
+            optimizer.zero_grad()
+            output = model(img)
+            loss = compute_loss(output, labels)
+            loss.backward()
+            optimizer.step()
+            post_processed_output = post_processor(output)
+            print(f"Epoch: {epoch}, Loss: {loss}")
+
+    
 
 
 def main(args):
     config = load_config(args.config)
     model = UNet(config)
     train_dataset = VOCSegmentationDataset(config, mode="train")
-    train_dataloader = DataLoader(train_dataset, batch_size=2, shuffle=True)
-    
+    train_dataloader = DataLoader(train_dataset, batch_size=4, shuffle=True)
+    train(model, train_dataloader)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
