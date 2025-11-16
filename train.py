84dca6e25effacabb26bf589546cc8fab24f1601
Alessio <alessiopiroli1@gmail.com>
2025-11-16 22:15:07 +0100

    Added function to train for one epoch

--- train.py
+++ train.py
@@ -2,34 +2,52 @@ from unet.model.u_net_model import UNet
 import argparse
 from unet.utils.misc import load_config
 import torch
-from unet.dataset.voc_dataset import VOCSegmentationDataset
+
+# from unet.dataset.voc_dataset import VOCSegmentationDataset
+from unet.dataset.oxford_pets_datset import OxfordPetsDataset
 from torch.utils.data import DataLoader
 from unet.utils.misc import plot_images_to_file
 import torch.nn as nn
 
 ###########################################
 import debugpy
-debugpy.listen(('localhost', 6001))
-print('Waiting for debugger attach...')
+
+debugpy.listen(("localhost", 6001))
+print("Waiting for debugger attach...")
 debugpy.wait_for_client()
 ###########################################
+
 
 def post_processor(output):
     output_sm = torch.nn.functional.softmax(output, 1)
     output_idxs = torch.argmax(output_sm, 1)
     return output_idxs
 
+
 def compute_loss(output, labels):
-    loss_fn = nn.CrossEntropyLoss(reduction='none')
+    loss_fn = nn.CrossEntropyLoss(reduction="none")
     loss = loss_fn(output, labels)
     mask_bg = labels == 0
     mask_fg = labels != 0
-    loss_bg = loss[mask_bg]*0.1
-    loss_fg = loss[mask_fg]*10
+    loss_bg = loss[mask_bg]
+    loss_fg = loss[mask_fg]
     loss_bg = torch.mean(loss_bg)
-    loss_fg = torch.mean(loss_bg)
+    loss_fg = torch.mean(loss_fg)
     total_loss = loss_bg + loss_fg
     return total_loss
+
+
+def train_one_epoch(model, train_dataloader, optimizer):
+    for img, labels in train_dataloader:
+        img, labels = img.cuda(), labels.cuda()
+        optimizer.zero_grad()
+        output = model(img)
+        loss = compute_loss(output, labels)
+        loss.backward()
+        optimizer.step()
+
+    return loss
+
 
 def train(model, train_dataloader):
     optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
@@ -37,25 +55,17 @@ def train(model, train_dataloader):
     model.train()
     n_epochs = 10
     for epoch in range(n_epochs):
-        for img, labels in train_dataloader:
-            img, labels = img.cuda(), labels.cuda()
-            optimizer.zero_grad()
-            output = model(img)
-            loss = compute_loss(output, labels)
-            loss.backward()
-            optimizer.step()
-            post_processed_output = post_processor(output)
-            print(f"Epoch: {epoch}, Loss: {loss}")
-
-    
+        loss = train_one_epoch(model, train_dataloader, optimizer)
 
 
 def main(args):
     config = load_config(args.config)
     model = UNet(config)
-    train_dataset = VOCSegmentationDataset(config, mode="train")
+    # train_dataset = VOCSegmentationDataset(config, mode="train")
+    train_dataset = OxfordPetsDataset("data/oxford_pets_dataset", "trainval")
     train_dataloader = DataLoader(train_dataset, batch_size=4, shuffle=True)
     train(model, train_dataloader)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
